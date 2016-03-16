84c4652aa68bc2fa47a30ef9f836d89e2e898999
Evandro Schuck <evandroschuck@hotmail.com>
2016-03-16 15:38:57 -0300

    Update calculeHours_forNode.js
    Correções

--- calculeHours_forNode.js
+++ calculeHours_forNode.js
@@ -1,37 +1,43 @@
-module.exports.hour = function(hour){
-  return {
-    isNegative: function(hours){
-      if(!hours) hours = hour;
-      return (hours.toString()[0] == '-' ? true : false);
-    },
-    toMinutes: function(hours){
-      if(!hours) hours = hour;
-      var hours    = hours.replace('-', '').split(':');
-      var minutes  = ((parseInt(hours[0]) * 60) + parseInt(hours[1]));
-      return (this.isNegative(hours) ? '-' : '') + minutes;
-    },
-    ofMinutes: function(minutes){
-      var min   = minutes.toString().replace('-', '');
-      var hours = (parseInt(min) / 60).toFixed();
-        min   = (parseInt(min) - (hours*60));
-      if(min < 0){
-        min   = parseInt(min) + 60;
-          hours = parseInt(hours) - 1;
-      }
-      return (this.isNegative(minutes) ? '-' : '')
-              + ((hours < 10 ? '0' : '') + hours + ':' + (min < 10 ? '0' : '') + min);
-    },
-    add: function(hours){
-      var result = parseInt(this.toMinutes())
-                 + parseInt(this.toMinutes(hours));
-          result = this.ofMinutes(result);
-      return result;
-    },
-    remove: function(hours){
-      var result = parseInt(this.toMinutes())
-                 - parseInt(this.toMinutes(hours));
-          result = this.ofMinutes(result);
-      return result;
+'use strict';
+module.exports.calc = function (info) {
+    if (!info) {
+        info = '00:00';
     }
-  }
-}
+    return {
+        isNegative: function (hours) {
+            if (!hours) {
+                hours = info;
+            }
+            return hours.toString()[0] === '-';
+        },
+        toMinutes: function (hours) {
+            if (!hours) {
+                hours = info;
+            }
+            var tmpHour = hours.replace('-', '').split(':');
+            var minutes = ((parseInt(tmpHour[0]) * 60) + parseInt(tmpHour[1]));
+            return (minutes !== 0 && this.isNegative(hours) ? '-' : '') + minutes;
+        },
+        ofMinutes: function (minutes) {
+            if (minutes === undefined) {
+                minutes = info;
+            }
+            var min = minutes.toString().replace('-', '');
+            var hours = (parseInt(min) / 60).toFixed();
+            min = (parseInt(min) - (hours * 60));
+            if (min < 0) {
+                min = parseInt(min) + 60;
+                hours = parseInt(hours) - 1;
+            }
+            return ((hours > 0 || min > 0) && this.isNegative(minutes) ? '-' : '') + ((hours < 10 ? '0' : '') + hours + ':' + (min < 10 ? '0' : '') + min);
+        },
+        add: function (hours) {
+            var result = parseInt(this.toMinutes()) + parseInt(this.toMinutes(hours));
+            return this.ofMinutes(result);
+        },
+        remove: function (hours) {
+            var result = parseInt(this.toMinutes()) - parseInt(this.toMinutes(hours));
+            return this.ofMinutes(result);
+        }
+    };
+};
